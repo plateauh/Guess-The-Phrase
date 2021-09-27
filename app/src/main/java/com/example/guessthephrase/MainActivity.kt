3dd291e5dee12afe44d46d3fee8ee7ec5a2174b2
plateauh <70834431+plateauh@users.noreply.github.com>
2021-09-27 11:40:31 +0300

    facing difficulties but im sure ill be fine inshallah

--- app/src/main/java/com/example/guessthephrase/MainActivity.kt
+++ app/src/main/java/com/example/guessthephrase/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.guessthephrase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.random.Random
+
+val listOfPhrases = Phrases().phrasesList // Phrases have separate class
+lateinit var guessList: ArrayList<String>
+lateinit var phraseView: TextView
+lateinit var recyclerView: RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Set RecyclerView
+        recyclerView = findViewById(R.id.rvGuesses)
+        recyclerView.adapter = RecyclerViewAdapter(guessList)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        phraseView = findViewById(R.id.tvPhrase)
+        val phrasePosition = setPhrase()
+    }
+
+    private fun setPhrase(): Int {
+        val phrasePosition = Random.nextInt(listOfPhrases.size)
+        val chosenPhrase = listOfPhrases[phrasePosition]
+        var phrase = ""
+        for (character in chosenPhrase){
+            phrase += if (character == ' ')
+                ' '
+            else
+                '*'
+        }
+        phraseView.text = "$phrase\nGuessed letters: "
+        return phrasePosition
     }
 }
---
+++ app/src/main/java/com/example/guessthephrase/Phrases.kt
@@ -0,0 +1,8 @@
+package com.example.guessthephrase
+
+class Phrases {
+    val phrasesList: List<String> = listOf(
+            "Najed Alseghair",
+            "Ruba Alseghair",
+            "Phrase3")
+}
